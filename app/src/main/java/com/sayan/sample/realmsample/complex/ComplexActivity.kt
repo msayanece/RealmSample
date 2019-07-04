999e42f687a7b44e97f68cc55a865b1c066c8b3e
sayan <anshu.p@encoders.co.in>
2019-07-04 16:35:20 +0530

    login activity layout ready

--- app/src/main/java/com/sayan/sample/realmsample/complex/ComplexActivity.kt
+++ app/src/main/java/com/sayan/sample/realmsample/complex/ComplexActivity.kt
@@ -1,12 +1,15 @@
 package com.sayan.sample.realmsample.complex
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import com.sayan.sample.realmsample.R
+import com.sayan.sample.realmsample.complex.loginrelated.LoginActivity
 
 class ComplexActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_complex)
+        startActivity(Intent(this, LoginActivity::class.java))
     }
 }
