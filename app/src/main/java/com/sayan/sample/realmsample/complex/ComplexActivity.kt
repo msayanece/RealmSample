cb39117b54a35bf68673bab5706774e2f027c485
sayan <anshu.p@encoders.co.in>
2019-07-04 15:11:45 +0530

    change complex activity to kotlin

---
+++ app/src/main/java/com/sayan/sample/realmsample/complex/ComplexActivity.kt
@@ -0,0 +1,12 @@
+package com.sayan.sample.realmsample.complex
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.sayan.sample.realmsample.R
+
+class ComplexActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_complex)
+    }
+}
